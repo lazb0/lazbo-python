8c6f168623e2be28506849b0a26a18f64f0e7ac9
Lukas Alois Zbornik <l.a.zbornik@gmail.com>
2021-11-04 21:46:00 +0100

    -

--- Fibonacci Sequence/mainFibSeq.py
+++ Fibonacci Sequence/mainFibSeq.py
@@ -1,10 +1,11 @@
 quantity = int(input('How many numbers you want to show? > '))
 
+print('First',quantity,'numbers of fibonacci sequence are: ')
 f1 = 0
 f2 = 1
 print('0, 1', end='')
 for i in range(quantity-2):
     f3 = f1 + f2
-    print(",",f3, end='')
+    print(',',f3, end='')
     f1 = f2
     f2 = f3
